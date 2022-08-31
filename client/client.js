10e285291a7a74f3c2200621df6097198903b846
= <=>
2022-08-31 15:52:19 -0600

    finished

---
+++ client/client.js
@@ -0,0 +1,45 @@
+const rBtn = document.querySelector('#RansomWare')
+
+
+const ransom = () => {
+    alert('Enjoy your new virus :)#%$j654j6j5------DownLoading...')
+}
+
+rBtn.addEventListener('click', ransom);
+
+const kBtn = document.querySelector('#KeyLogger')
+
+
+const logger = () => {
+    alert('Enjoy your new virus :)#%$j654j6j5------DownLoading...')
+}
+
+kBtn.addEventListener('click', logger);
+
+
+const mBtn = document.querySelector('#Mydoom')
+
+
+const doom = () => {
+    alert('Enjoy your new virus :)#%$j654j6j5------DownLoading...')
+}
+
+mBtn.addEventListener('click', doom);
+
+const iBtn = document.querySelector('#ILOVEYOU')
+
+
+const loveYou = () => {
+    alert('Enjoy your new virus :)#%$j654j6j5------DownLoading...')
+}
+
+iBtn.addEventListener('click', loveYou);
+
+const wBtn = document.querySelector('#WannaCry')
+
+
+const wanna = () => {
+    alert('Enjoy your new virus :)#%$j654j6j5------DownLoading...')
+}
+
+wBtn.addEventListener('click', wanna);
